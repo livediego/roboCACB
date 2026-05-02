c02df1123aa99d7b97bdad4d19537c27e7940499
livediego@gmail.com <livediego@gmail.com>
2026-05-02 20:36:04 -0300

    correção páginas busca 7

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ async function buscarEmpresaNaGrid(page, nomeEmpresa, maxPaginas = 2) {
     const primeiraPagina = Math.max(0, total - maxPaginas) + 1;
     console.log("Primeira página:", primeiraPagina);
 
-    for (let i = total - 1; i >= primeiraPagina; i--) {
+    for (let i = total; i >= primeiraPagina; i--) {
 
         await paginas.nth(i).click();
         await wait(page, 2000);
